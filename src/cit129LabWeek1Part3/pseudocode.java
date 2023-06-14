4eb13899bef32d6299c06288d09cc7be930faba9
HOME_MAC <Gaby@BCs-iMac.local>
2023-06-13 22:08:27 -0700

    questions 12 and 13

--- src/cit129LabWeek1Part3/pseudocode.java
+++ src/cit129LabWeek1Part3/pseudocode.java
@@ -17,22 +17,104 @@ public class pseudocode {
 /*For your answers below, just state the line number. If none, write N/A.
 a. What are the input statements?
 
+Ans: 
 
+there are no inputs
 
 b. What are the output statements?
 
 Ans:
+
 The number we are add in this case
 Num1 and Num2
 
 
 c. What are the assignment statements?
+the assignment statments are num2 = num1 + num2
+
+set Num1 = 5
+Set Num1 = Num1 + 3
+Set Num2 = 5
+Num2 = Num1 + Num2
+Set Num1 = 10 
+
 d. What number is displayed by this pseudocode? Here, I need the value that will be displayed.
+Ans: 
+values  
+num1 which is 10 
+also
+ num2 which is 13
 
 	 */
 		
 		
+		/*Question 11 : 
+		 * 
+		 * Write a pseudocode for a program to ask the user for a measurement in feet. Convert the measurement in feet to meters and display the result. Look up the conversion from feet to meters. Don’t forget to create descriptive variable names
+CIT129 – Assignment 1 – Chapter 0, 1, 3 Page 2
+  
+and number your steps. Think about the Input, processing and output steps needed
+to solve this problem. YOU ARE NOT writing Raptor code for this exercise.
+
+		 */
+/*
+ *
+ * Start 
+ * 
+ * Write " Enter feet to convert into meters
+ * 
+ *   Set  feet
+ * 	
+ * Set meters = feet * 0.3048
+ * 
+ * Write " Your converstion to meters is: " 
+ * 
+ * Write meters
+ * 
+ * End
+ * 
+ * 
+ * 
+ * 		
+ */
 		
+		
+		
+		/*
+		 * Write a pseudocode for the following problem helping a restaurant owner. Ask
+           the user for the subtotal of a bill and the gratuity rate. Compute the amount of gratuity and total.
+           For example, if the user enters 10 for subtotal and 15% for gratuity rate, the program displays 1.5 
+           for the gratuity amount and 11.5 for the total. 
+		 * 
+		 * 
+		 */
+		
+		/*Start
+		 * 
+		 * Write "Enter Subtotal: " 
+		 * Set Subtotal
+		 * 
+		 * Write "Gradtituty rate : " 
+		 * Set rate
+		 * 
+		 * Set decimal = rate / 100
+		 * 
+		 * Set gratuity= subtotal * decimal
+		 * 
+		 * set total = subtotal + gratutity 
+		 * 
+		 * Write " the gradtutiy amount is: " 
+		 * 
+		 * Write gratuity
+		 * Wrtie " The total is : " 
+		 * Write total 
+		 * 
+		 * End 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
 		
 		
 	}
